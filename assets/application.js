536dcdf65739d68091687b677cdc8fdb7a470c4f
xavier barthelet <kerahan@xaviers-MacBook-Pro.local>
2019-07-24 23:04:49 +0900

    modal fixed

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,21 @@
-// Put your applicaiton javascript here
+// $(function() {
+// 	document.querySelector('.bg-modals').style.display = 'flex';
+// });
+
+// document.querySelector('.close').addEventListener('click', function() {
+// 	document.querySelector('.bg-modals').style.display = 'none';
+// });
+
+// document.getElementById('closeBtn').addEventListener('click', function() {
+// 	document.querySelector('.bg-modals').style.display = 'none';
+// });
+
+// window.addEventListener('click', function(e) {
+// 	if (e.target == document.querySelector('.bg-modals')) {
+// 		document.querySelector('.bg-modals').style.display = 'none';
+// 	}
+// });
+
+$(function() {
+    $('#exampleModalCenter').modal('show');
+});
